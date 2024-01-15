61f936714bbd32e5a052fcc83075df227e98ed2e
HYWSXK <2846597446@qq.com>
2024-01-15 17:28:02 +0800

    pc通信发送角度修改

--- 步兵12.29/步兵/standard-robot-main/standard-robot-main/PROTOCOL/pc_tx_data.c
+++ 步兵12.29/步兵/standard-robot-main/standard-robot-main/PROTOCOL/pc_tx_data.c
@@ -98,22 +98,22 @@ void get_infantry_info(void)
 			{
 				pc_send_mesg.task_mode = SMALL_BUFF_MODE;		//小能量机关
         //发送编码位角度？看视觉需求，用电机还是陀螺仪
-        pc_send_mesg.robot_pitch = -gimbal.sensor.pit_relative_angle;
+        pc_send_mesg.robot_pitch = gimbal.sensor.pit_relative_angle;
         pc_send_mesg.robot_yaw =  gimbal.sensor.yaw_relative_angle;
 			}
       else if(gimbal.big_buff_ctrl)
 			{
         //发送编码位角度？看视觉需求，用电机还是陀螺仪
 				pc_send_mesg.task_mode = BIG_BUFF_MODE;			//大能量机关
-        pc_send_mesg.robot_pitch = -gimbal.sensor.pit_relative_angle;
+        pc_send_mesg.robot_pitch = gimbal.sensor.pit_relative_angle;
         pc_send_mesg.robot_yaw =  gimbal.sensor.yaw_relative_angle;
 			}
 			else
 			{
         //发送编码位角度？看视觉需求，用电机还是陀螺仪
 				pc_send_mesg.task_mode = NORMAL_CTRL_MODE;	//普通模式		
-        pc_send_mesg.robot_pitch = -gimbal.sensor.pit_relative_angle;
-        pc_send_mesg.robot_yaw =  gimbal.sensor.yaw_relative_angle;
+        pc_send_mesg.robot_pitch = gimbal.sensor.pit_relative_angle;
+        pc_send_mesg.robot_yaw =  gimbal.sensor.yaw_gyro_angle;
 			}
 		}break;
 		
